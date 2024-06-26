b0dc1c492abbb248e73d4b892ea31099367d1c53
ottocsb <w2606662632@gmail.com>
2024-06-26 14:21:53 +0800

    chore: up tarui dep

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-fn main() {
+fn main() { 
   tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+      .run(tauri::generate_context!())
+      .expect("error while running tauri application");
 }
